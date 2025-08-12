d25f0cc474a7c802ee8bdb13f80dcfc774913165
SurajYadav8 <raosahab1e@gmail.com>
2025-08-12 18:00:22 +0000

    rebase & Vault

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,20 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-contract Vault {
+
+import {IRebaseToken} from "./interfaces/iRebaseToken.sol";
+
+
+contract Vault  {
     // we need to pass the token address to the constructor
     // create a deposit function that mint tokens to the user to the amount of ETH the user
     // create a redeem function that burns tokens from the user and sends the user ETH
     // create a way to add rewards to the vault
-    address private immutable i_rebaseToken;
+    IRebaseToken private immutable i_rebaseToken;
 
-    constructor (address _rebaseToken) {
+    event Deposit(address indexed user, uint256 amount);
+    event Redeem(address indexed user, uint256 amount);
+
+    error Vault__RedeemFailed();
+
+    constructor (IRebaseToken _rebaseToken) {
         i_rebaseToken = _rebaseToken;
     }
 
     receive() external payable {}
 
+    /**
+     * @notice Allows users to deposit ETH into the vault and mint rebase tokens in return
+     */
+
+
+    function deposit() external payable {
+        // 1. we need to user the amount of ETH the user has sent to mint tokens to the user
+        i_rebaseToken.mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    /**
+     * @notice Allows users to redeem their rebase tokens for ETH
+     * @param _amount The amount of rebase tokens to redeem
+     */
+
+    function redeem(uint256 _amount) external {
+        // 1. burn the tokens from the user
+        i_rebaseToken.burn(msg.sender, _amount);
+        // 2. We need to send the user ETH
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
+        if(!success) {
+            revert Vault__RedeemFailed();
+        }
+        emit Redeem(msg.sender, _amount);
+    }
+
+    /** */
+
     function getRebaseTokenAddress()external view returns(address) {
-       return i_rebaseToken;
+       return address(i_rebaseToken);
     }
 }
