ac27b1d8cf719e7e4b6554f6fa862e52ce767cca
SurajYadav8 <raosahab1e@gmail.com>
2025-08-23 18:50:01 +0000

    fmt

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -5,5 +5,7 @@ import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 // import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 abstract contract RebaseTokenPool is TokenPool {
-     constructor(IERC20 _token, address[] memory _allowlist, address _rnmProxy, address _router) TokenPool(_token, _allowlist, _rnmProxy, _router){}
+    constructor(IERC20 _token, address[] memory _allowlist, address _rnmProxy, address _router)
+        TokenPool(_token, _allowlist, _rnmProxy, _router)
+    {}
 }
