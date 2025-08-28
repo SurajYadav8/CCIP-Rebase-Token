644069969627db3dce51c69f401038a89c1804be
SurajYadav8 <raosahab1e@gmail.com>
2025-08-28 13:23:25 +0000

    chainupdate

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -84,18 +84,19 @@ contract CrossChainTest is Test {
     }
 
     function configureTokenPool(uint256 fork, address localPool, uint64 remoteChainSelector) public {
-        vm.selectFor(fork);
+        vm.selectFork(fork);
         vm.prank(owner);
         TokenPool.ChainUpdate[] memory chainsToAdd = new TokenPool.ChainUpdate[](1);
-        // struct ChainUpdate {
-        //     uint64 remoteChainSelector;
-        //     bytes[] remotePoolAddresses;
-        //     bytes remoteTokenAddress;
-        //     RateLimiter.Config outboundRateLimiterConfig;
-        //     RateLimiter.Config inboundRateLimiterConfig;
-        // }
+        //         struct ChainUpdate {
+        //     uint64 remoteChainSelector; // ──╮ Remote chain selector
+        //     bool allowed; // ────────────────╯ Whether the chain should be enabled
+        //     bytes remotePoolAddress; //        Address of the remote pool, ABI encoded in the case of a remote EVM chain.
+        //     bytes remoteTokenAddress; //       Address of the remote token, ABI encoded in the case of a remote EVM chain.
+        //     RateLimiter.Config outboundRateLimiterConfig; // Outbound rate limited config, meaning the rate limits for all of the onRamps for the given chain
+        //     RateLimiter.Config inboundRateLimiterConfig; // Inbound rate limited config, meaning the rate limits for all of the offRamps for the given chain
+        //   }
+
         chainsToAdd[0] = TokenPool.ChainUpdate({});
         TokenPool(localPool).applyChainUpdates(new uint64[](0), chainsToAdd);
-
     }
 }
