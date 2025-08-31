d966c0155c2da63b63b8981bf25c4949b2913600
SurajYadav8 <raosahab1e@gmail.com>
2025-08-31 17:00:50 +0000

    script

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -4,12 +4,23 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {Vault} from "../src/Vault.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
+import {CCIPLocalSimulatorFork} from "../lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {IERC20} from "../lib/ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
 
 
 
 contract TokenAndPoolDeployer is Script{
 
-    function 
+    function run() public returns(RebaseToken token, RebaseTokenPool pool) {
+        CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        vm.startBroadcast();
+        token = new RebaseToken();
+        pool = new RebaseTokenPool();
+        vm.stopBroadcast();
+    }
 }
 
 contract VaultDeployer is Script {
